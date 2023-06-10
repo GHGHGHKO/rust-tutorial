93b92b8a6935cdd85e66f5407dcb4c095c8d036a
GHGHGHKO <gudrb963@gmail.com>
2023-06-10 22:11:22 +0900

    feat: match `find_prime_number` example

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -4,3 +4,17 @@ A
 :
 
 Cargo.lock,1\4\14a5af756bb04b4a26c80cf95acf32f0f8b9f30f
+:
+
+Cargo.toml,1\b\1b290eb385892bfd4870c08a785598e98c8691b7
+M
+src/client/http_bin_client.rs,7\7\775b391cf1bfe6082368b745928f8a5af9414c8f
+L
+src/dto/http_bin_response.rs,4\b\4b17a5dcce7d4730c8d2af95899b3724b0c71fc5
+:
+
+src/dto.rs,f\8\f85014b9ecc4ba4e31fa7e3a91ee741803d6e6b2
+I
+src/dto/tenor_response.rs,d\a\dae4c375e23210e5f884d5f28d9a11a47e2cec56
+=
+src/client.rs,7\e\7e3ff8705aaef26f38ac296b96428852959e67c4
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,24 @@ mod client;
 use client::http_bin_client::http_bin_client;
 use client::tenor_client::tenor_client;
 
+fn find_prime_number() {
+    let number = 7;
+
+    match number {
+        1 => println!("It's one!"),
+
+        2 | 3 | 5 => println!("It's a prime number!"),
+
+        4..=10 => println!("It's between 4 and 10 (inclusive)!"),
+
+        n if n % 2 == 0 => println!("It's an even number!"),
+
+        _ => println!("It doesn't match any condition."),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    find_prime_number();
     let _ = http_bin_client();
     tenor_client()
 }
