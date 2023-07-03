9d5e3e9ce96e31d531f28cb29ac05f9759b832fb
GHGHGHKO <gudrb963@gmail.com>
2023-07-03 14:01:13 +0900

    feat: change `fail_move_ownership` example

--- src/ownership/ownership.rs
+++ src/ownership/ownership.rs
@@ -1,17 +1,17 @@
 pub fn fail_move_ownership() {
-    let i_am_on_stack: &str = "7427466391.com";
+    let i_am_on_stack: i64 = 7427466391;
     let me_too = i_am_on_stack;
 
     println!("i_am_on_stack is {}", i_am_on_stack);
     println!("me_too is {}", me_too);
 
-    let i_am_on_heap = String::from("Ferris");
-    print_function(i_am_on_heap.clone());
+    let i_am_on_heap = vec![500, 60000];
+    print_function(&i_am_on_heap);
 
-    let me_too = i_am_on_heap.clone();
-    print_function(me_too);
+    let me_too = i_am_on_heap;
+    print_function(&me_too);
 }
 
-fn print_function(name: String) {
-    println!("{}", name);
+fn print_function(params: &Vec<i32>) {
+    println!("{:?}", &params);
 }
